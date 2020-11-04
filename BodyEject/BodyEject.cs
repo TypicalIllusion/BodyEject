ee24f1c33407aadd8983190e109a45863445dc51
TypicalIllusion <shrimp001x@gmail.com>
2020-11-04 08:46:47 -0500

    Updated to 2.1.13

--- BodyEject/BodyEject.cs
+++ BodyEject/BodyEject.cs
@@ -12,8 +12,8 @@ namespace BodyEject
         private Handlers.Server server = new Handlers.Server();
         public override string Name { get; } = "BodyEject";
         public override string Author { get; } = "TypicalIllusion";
-        public override Version Version { get; } = new Version(1, 5, 2);
-        public override Version RequiredExiledVersion { get; } = new Version(2, 1, 11);
+        public override Version Version { get; } = new Version(1, 5, 0);
+        public override Version RequiredExiledVersion { get; } = new Version(2, 1, 3);
         public override string Prefix { get; } = "BodyEject";
 
         public static BodyEject Instance;
